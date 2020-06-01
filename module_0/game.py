7c9854f0986de078de4aa01ea14d60cb089a0e65
Petr Rusanov <peter.rusanov@gmail.com>
2020-06-01 11:01:23 +0200

    game logic documentation added

--- module_0/game.py
+++ module_0/game.py
@@ -2,6 +2,17 @@ import math
 
 
 def guess(number):
+    """Function accepts a number and tries to guess it using as less attempts as possible.
+    It is using divide and conquer logic, so it tries to find the range where the number is
+    and ignores other ranges.
+
+    Args:
+        number (int): A number to guess
+
+    Returns:
+        int: Number of attempts used to guess the number
+    """
+
     count = 1
 
     low = 1
