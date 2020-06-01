979b70fadcc38c546ee9dd30890e175791c43922
Petr Rusanov <peter.rusanov@gmail.com>
2020-06-01 10:59:05 +0200

    game logic implemented

---
+++ module_0/game.py
@@ -0,0 +1,22 @@
+import math
+
+
+def guess(number):
+    count = 1
+
+    low = 1
+    high = 100
+
+    predict = high / 2
+
+    while number != predict:
+        count += 1
+
+        if number > predict:
+            low = predict
+        elif number < predict:
+            high = predict
+
+        predict = math.floor((high + low) / 2)
+
+    return count
