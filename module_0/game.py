ba90ad3a7dbded40ee19cf1fc8820fdfd314520e
Petr Rusanov <peter.rusanov@gmail.com>
2020-06-01 11:08:58 +0200

    document game logic function code

--- module_0/game.py
+++ module_0/game.py
@@ -18,16 +18,16 @@ def guess(number):
     low = 1
     high = 100
 
-    predict = high / 2
+    predict = high / 2  # first prediction is half of the highest possible number
 
     while number != predict and number != low and number != high:
         count += 1
 
-        if number > predict:
+        if number > predict:  # if number is higher than prediction, then we can ignore lower numbers
             low = predict
-        elif number < predict:
+        elif number < predict:  # if number is lower than prediction, then we can ignore higher numbers
             high = predict
 
-        predict = math.floor((high + low) / 2)
+        predict = math.floor((high + low) / 2)  # divide range between low and high to get new prediction
 
     return count
